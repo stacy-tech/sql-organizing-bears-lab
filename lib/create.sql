c32c098737cebbccd47180b387fa59915928c412
stacy <stacydn23@gmail.com>
2021-01-08 20:55:39 -0500

    commit tree b0fdcb6d0d44c4a06d8e3e1f3f05d105b3c18d52

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name text, 
+    age integer, 
+    gender text,
+    sex text,
+    color text,
+    temperament text,
+    alive boolean
+);
---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 4, "m", "black", "cool", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 6, "m", "brown", "cool", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 3, "f", "white", "cool", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 2, "m", "gray", "warm", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 1, "f", "black", "fast", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 5, "m", "black and white", "wrong", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 2, "f", "white", "warm", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (null, 3, "m", "white", "magenta", 1);
